2b6073343a0c9c41ce85c090e7a793a5e93bdd95
Michovica <85302040+Michovica@users.noreply.github.com>
2024-05-08 19:36:19 +0200

    comments ajax adding

--- IKTProject1/Controllers/MessagesController.cs
+++ IKTProject1/Controllers/MessagesController.cs
@@ -1,7 +1,10 @@
 ﻿using IKTProject1.Data;
 using IKTProject1.Models;
+using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Data.SqlClient;
+using System.Text.Json;
 
 namespace IKTProject1.Controllers
 {
@@ -16,10 +19,25 @@ namespace IKTProject1.Controllers
 
         public IActionResult Index()
         {
+
             //get list of all messages
-            var mesList = _context.Messages.ToList();
+            List<MessageModel>? mesList = _context.Messages.ToList();
+            mesList.Reverse();
 
             return View("Index", new {MessagesList = mesList});
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult PublishComment(string comment)
+        {
+            //check if user is authenticated
+            if(!User.Identity.IsAuthenticated)
+            {
+                return Content(JsonSerializer.Serialize(new { status = "fail" }));
+            }
+
+            return Content(JsonSerializer.Serialize(new { status = "good" }));
         }
 
         //on get display createForm
