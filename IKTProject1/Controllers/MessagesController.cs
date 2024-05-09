79debdfede4d845887bbb66ed08ac001a13cde8c
Michovica <85302040+Michovica@users.noreply.github.com>
2024-05-09 20:32:48 +0200

    sqlite working

--- IKTProject1/Controllers/MessagesController.cs
+++ IKTProject1/Controllers/MessagesController.cs
@@ -19,7 +19,6 @@ namespace IKTProject1.Controllers
 
         public IActionResult Index()
         {
-
             //get list of all messages
             List<MessageModel>? mesList = _context.Messages.ToList();
             mesList.Reverse();
@@ -29,11 +28,30 @@ namespace IKTProject1.Controllers
 
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public IActionResult PublishComment(string comment)
+        public IActionResult PublishComment(string comment, string messageId)
         {
             //check if user is authenticated
             if(!User.Identity.IsAuthenticated)
             {
+                return Content(JsonSerializer.Serialize(new { status = "fail" }));
+            }
+
+            try
+            {//save to database
+
+                //create comment object
+                var comm = new CommentModel();
+                comm.Text = comment;
+                comm.Autor = User.Identity.Name;
+                comm.MessageId = int.Parse(messageId);
+
+                //save to database
+                _context.Comments.Add(comm);
+                _context.SaveChanges();
+            }
+            catch
+            {
+                //return error
                 return Content(JsonSerializer.Serialize(new { status = "fail" }));
             }
 
